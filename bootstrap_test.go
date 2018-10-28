70ccceac93a718c53b2bc7b8bc6d5648cf18e8ff
igcfs <igcfs.org@gmail.com>
2018-10-28 17:59:42 +0100

    Dummy tests to for CI.

--- bootstrap_test.go
+++ bootstrap_test.go
@@ -4,7 +4,17 @@ import "testing"
 
 func Test_processCmdLn(t *testing.T) {
 	o := processCmdLn()
-	if len(o) > 0 {
+	if len(o) > 0 { // TODO dummy test, replace it.
 		t.Error()
+	}
+}
+
+func Test_initiateHost_noOptions(t *testing.T) {
+	options := []Option{} // no options
+	_, c, err := initiateHost(options)
+	defer c()
+
+	if err != nil {
+		t.Error(err)
 	}
 }
